48301e8474020bf84a5386c1296eed508c7c3a58
Sergey Melnychuk <sergey-melnychuk@users.noreply.github.com>
2025-10-19 21:58:24 +0200

    feat: full A/B runner ready!

--- examples/runner.rs
+++ examples/runner.rs
@@ -1,6 +1,9 @@
 use std::time::Instant;
 use std::{pin::Pin, sync::Arc};
 
+use evm_tracer::alloy_eips::BlockNumberOrTag;
+use evm_tracer::alloy_provider::{Provider, ProviderBuilder};
+use evm_tracer::alloy_rpc_types::BlockTransactions;
 use tokio::sync::Mutex;
 
 use solenoid::{common::{block::Block, word::Word}, eth, ext::Ext};
@@ -12,9 +15,9 @@ use solenoid::{
 
 use evm_tracer::{OpcodeTrace, run::TxResult};
 
-fn as_tx_result(value: CallResult<LoggingTracer>) -> TxResult {
+fn as_tx_result(gas_costs: i64, value: CallResult<LoggingTracer>) -> TxResult {
     TxResult {
-        gas: value.evm.gas.used as u64, // TODO: use finalized gas
+        gas: value.evm.gas.finalized(gas_costs) as u64,
         ret: value.ret,
         rev: value.evm.reverted,
     }
@@ -26,6 +29,16 @@ pub fn runner(
 ) -> impl FnMut(Tx) -> Pin<Box<dyn Future<Output = eyre::Result<(TxResult, Vec<OpcodeTrace>)>>>> {
     let ext = Arc::new(Mutex::new(ext));
     move |tx| {
+        let call_cost = 21000i64;
+        let data_cost = {
+            let total_calldata_len = tx.input.as_ref().len();
+            let nonzero_bytes_count =
+                tx.input.as_ref().iter().filter(|byte| *byte != &0).count();
+            nonzero_bytes_count * 16 + (total_calldata_len - nonzero_bytes_count) * 4
+        } as i64;
+        let create_cost = 32000i64;
+        let init_code_cost = 2 * tx.input.as_ref().len().div_ceil(32) as i64;
+
         let header = header.clone();
         let ext = ext.clone();
         Box::pin(async move {
@@ -43,6 +56,13 @@ pub fn runner(
                 Ok::<_, eyre::Report>(result)
             }).await??;
 
+            let gas_costs = if tx.to.is_some() {
+                call_cost + data_cost
+            } else {
+                let deployed_code_cost = 200 * result.ret.len() as i64;
+                call_cost + data_cost + create_cost + init_code_cost + deployed_code_cost
+            };
+
             let traces = result
                 .tracer
                 .take()
@@ -50,7 +70,7 @@ pub fn runner(
                 .filter_map(|event| evm_tracer::OpcodeTrace::try_from(event).ok())
                 .collect::<Vec<_>>();
 
-            Ok((as_tx_result(result), traces))
+            Ok((as_tx_result(gas_costs, result), traces))
         })
     }
 }
@@ -58,36 +78,77 @@ pub fn runner(
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     dotenv::dotenv().ok();
-    let _ = tracing_subscriber::fmt::try_init();
+    tracing_subscriber::fmt::init();
 
     let url = std::env::var("URL")?;
     let eth = eth::EthClient::new(&url);
 
-    let block_number = std::env::args()
-        .nth(1)
-        .and_then(|number| number.parse::<u64>().ok())
-        .unwrap_or(23027350); // https://xkcd.com/221/
+    let block_number = std::env::args().nth(1);
+    let block_number = if block_number.as_ref().map(|s| s.as_str()) == Some("latest") {
+        let (block_number, _) = eth.get_latest_block().await?;
+        block_number
+    } else {
+        block_number
+            .and_then(|number| number.parse::<u64>().ok())
+            .unwrap_or(23027350) // https://xkcd.com/221/
+    };
 
     let Block{ header, transactions } = eth.get_full_block(Word::from(block_number)).await?;
-    eprintln!("📦 Fetched block number: {} [with {} txs]", header.number.as_usize(), transactions.len());
+    println!("📦 Fetched block number: {} [with {} txs]", header.number.as_usize(), transactions.len());
 
     let ext = Ext::at_number(Word::from(block_number - 1), eth).await?;
 
     let mut f = runner(header, ext);
 
-    for tx in transactions {
-        let idx = tx.index.as_usize();
+    let provider = ProviderBuilder::new().connect_http(url.parse()?);
+    let block = match provider
+        .get_block_by_number(BlockNumberOrTag::Number(block_number))
+        .full()
+        .await
+    {
+        Ok(Some(block)) => block,
+        Ok(None) => eyre::bail!("Block not found"),
+        Err(error) => eyre::bail!("Error: {:?}", error),
+    };
+
+    let BlockTransactions::Full(txs) = block.transactions else {
+        eyre::bail!("Expected full block");
+    };
+    println!("📦 Fetched block number: {} [with {} txs]", block.header.number, txs.len());
+
+    let mut g = evm_tracer::run::runner(block.header, provider);
+
+    let len = transactions.len();
+    assert_eq!(txs.len(), len);
+
+    for idx in 0..len {
+        println!("---\n### block={block_number} index={idx} hash={}", txs[idx].info().hash.unwrap_or_default());
+
+        let tx = txs[idx].clone();
         let now = Instant::now();
-        match f(tx).await {
+        let (result, traces) = g(tx)?;
+        let ms = now.elapsed().as_millis();
+        println!("REVM \tOK={} \tRET={} \tGAS={} \tTRACES={} \tms={ms}", 
+            !result.rev,
+            true,
+            result.gas,
+            traces.len());
+        let ret = result.ret;
+
+        let tx = transactions[idx].clone();
+        let now = Instant::now();
+        let r = f(tx).await;
+        let ms = now.elapsed().as_millis();
+        match r {
             Ok((result, traces)) => {
-                let ms = now.elapsed().as_millis();
-                eprintln!("TX \tindex={idx} \tOK={} \tGAS={:03} \tTRACES={:03} \tms={ms}", 
-                    !result.rev, 
+                println!("sole \tOK={} \tRET={} \tGAS={} \tTRACES={} \tms={ms}", 
+                    !result.rev,
+                    result.ret == ret,
                     result.gas,
                     traces.len());
             }
             Err(e) => {
-                eprintln!("TX \tindex={idx} \tPANIC: '{e}'"); 
+                println!("sole \tPANIC: '{e}'"); 
             }
         }
     }
